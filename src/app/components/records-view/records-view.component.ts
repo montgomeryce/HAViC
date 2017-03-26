08d64701c6f2cade55ee315e77919644f168abae
Chad Montgomery <montgomery.chad.e@gmail.com>
2017-03-26 19:10:11 -0400

    cleanup

--- src/app/components/records-view/records-view.component.ts
+++ src/app/components/records-view/records-view.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import {ActivityService}         from '../../services/activity.service';
-import {Activity} from "../../models/activity";
+import {ActivityService} from '../../services/activity.service';
+import {Activity} from '../../models/activity';
 
 @Component({
   selector: 'app-records-view',
@@ -8,18 +8,20 @@ import {Activity} from "../../models/activity";
   styleUrls: ['./records-view.component.css']
 })
 export class RecordsViewComponent implements OnInit {
-    calBurnActs:Activity[];
-    longestActs:Activity[];
+    calBurnActs: Activity[];
+    longestActs: Activity[];
 
-    constructor(private activityService:ActivityService) {
+    constructor(private activityService: ActivityService) {
 
     }
 
     getData() {
-        this.activityService.getActivities('totalCalories','desc',6).then(
+
+        // todo: the component itself should be getting this data... need to refactor
+        this.activityService.getActivities('totalCalories', 'desc', 6).then(
             data => this.calBurnActs = data
         );
-        this.activityService.getActivities('totalDistance','desc',6).then(
+        this.activityService.getActivities('totalDistance', 'desc', 6).then(
             data => this.longestActs = data
         );
     }
